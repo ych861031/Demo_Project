06ee568253c41723ba002b7a49a718de1280c68c
DESKTOP-VCTDNR3\user <jsbach4770@gmail.com>
2018-06-15 16:01:23 +0800

    美編"登入後"畫面+gridview元件 ps.登出鍵尚未完成

--- app/src/main/java/com/example/yangchunghsuan/demo_project/PersonalFragment.java
+++ app/src/main/java/com/example/yangchunghsuan/demo_project/PersonalFragment.java
@@ -3,12 +3,20 @@ package com.example.yangchunghsuan.demo_project;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -70,6 +78,8 @@ public class PersonalFragment extends Fragment {
         }
     }
 
+
+    public String userName;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -82,6 +92,28 @@ public class PersonalFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(view.getContext(),"Personal Page",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FirebaseDatabase storge = FirebaseDatabase.getInstance();
+        DatabaseReference storageRef = storge.getReference("user/4/userName");
+        storageRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try{
+                    userName = dataSnapshot.getValue().toString();
+                    TextView tv_username;
+                    tv_username =view.findViewById(R.id.tv_username);
+                    tv_username.setText(userName+"您好");
+
+                }catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
         //回傳這個view讓MainActivity更改fragment
